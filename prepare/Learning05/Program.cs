feac174e17a36f950e0f9c6b1fb6e9d9481e035f
AlexanderArj <aromeroj20@gmail.com>
2024-10-21 19:37:23 -0500

    Inheritance

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment1 = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment derivadas = new MathAssignment("Alexander", "Differential Calculus", "Chapters 4-5", "Problems 8-21" );
+
+        Console.WriteLine(derivadas.GetSummary());
+        Console.WriteLine(derivadas.GetHomeworkList());
+
+        WritingAssignment history = new WritingAssignment("Carlos","European History","The causes of World War II");
+
+        Console.WriteLine(history.GetSummary());
+        Console.WriteLine(history.GetWritingInformation());
+
     }
+
 }
